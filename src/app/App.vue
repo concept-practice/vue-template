7a6b7a78029072bb972f20b743028b491338b728
Michael Bradvica <mjbradvica@protonmail.com>
2022-05-06 14:58:33 -0400

    small changes

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <router-view />
+  <HomeView />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import HomeView from "@/features/home/HomeView.vue";
 
 export default defineComponent({
   name: "App",
+  components: {
+    HomeView,
+  },
 });
 </script>
