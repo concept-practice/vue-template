abeeecd95a22445e520a58649a348fe833934d23
Michael Bradvica <mjbradvica@protonmail.com>
2022-05-13 00:14:30 -0400

    lint and settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "@typescript-eslint/explicit-function-return-type": [1],
   },
   overrides: [
     {
