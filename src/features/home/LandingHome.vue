21199931b500953fbf42b1833d1e9b495523acd3
Michael Bradvica <mjbradvica@protonmail.com>
2022-12-05 19:44:51 -0500

    testing actually works

--- src/features/home/LandingHome.vue
+++ src/features/home/LandingHome.vue
@@ -1,5 +1,17 @@
 <template>
   <p>At Home</p>
+  <p>{{ counter }}</p>
+  <div>
+    <button @click="handleUpdateCounter">Increase</button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const counter = ref<number>(0);
+
+const handleUpdateCounter = (): void => {
+  ++counter.value;
+};
+</script>
--- src/tests/home/LandingHome.spec.ts
+++ src/tests/home/LandingHome.spec.ts
@@ -1,10 +1,21 @@
 import { render, screen } from "@testing-library/vue";
 import LandingHome from "../../features/home/LandingHome.vue";
+import userEvent from "@testing-library/user-event";
 
 describe("LandingHome", () => {
   it("Renders", () => {
     render(LandingHome);
 
-    screen.getByText(/At Home/i);
+    expect(screen.getByText(/At Home/i)).toBeInTheDocument();
+  });
+
+  it("Counter Works", async () => {
+    render(LandingHome);
+
+    const user = userEvent.setup();
+
+    user.click(screen.getByText(/Increase/i));
+
+    expect(await screen.findByText(/1/i)).toBeInTheDocument();
   });
 });
